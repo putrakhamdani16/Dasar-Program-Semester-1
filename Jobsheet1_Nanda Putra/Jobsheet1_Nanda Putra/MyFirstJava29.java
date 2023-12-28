7063fabea0bc226991db44f1656131216f5a27f7
Putra Khamdani <121760866+putrakhamdani16@users.noreply.github.com>
2023-12-28 14:01:01 +0700

    Add files via upload

---
+++ Jobsheet1_Nanda Putra/Jobsheet1_Nanda Putra/MyFirstJava29.java
@@ -0,0 +1,8 @@
+public class MyFirstJava29 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Nanda Putra");
+
+        //System.out.print("\nSaya Adalah Mahasiswa Prodi Teknik Informatika");
+
+    }
+}
